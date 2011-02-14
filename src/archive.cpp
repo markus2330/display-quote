6bccb317dceb8ae9f10298270c8f48fbe52b628b
Markus Raab <uni@markus-raab.org>
2011-02-14 11:25:58 +0100

    now parse multiple markers

--- src/archive.cpp
+++ src/archive.cpp
@@ -10,30 +10,52 @@ Archive::Archive(std::string filename)
 {
 	ifstream ifs (filename.c_str());
 	int posnr = 1;
-	string position = "1";
 	quotes.push_back("");
 
 	string current;
 	while (getline (ifs, current))
 	{
-		int linepos = current.size()-position.size();
-		cout << current << " rfind: " << current.rfind(position)
-			<< " size: " << current.size() 
-			<< " linepos: " << linepos << endl;
-		if (current.size() >= linepos &&
-		    current.rfind(position) == linepos &&
-		    (current[linepos-1] == ' ' || current[linepos-1] == '.'))
+		vector<int> markers;
+
+		cout << "now parsing: " << current << endl;
+		string::reverse_iterator it = current.rbegin();
+		for(; it != current.rend();)
 		{
-			cout << position << " found" << endl;
-			++posnr;
-			std::ostringstream str;
-			str << posnr;
-			position = str.str();
-			cout << "now search for : " << position
-				<< " nr: " << posnr
-				<< " str: " << str.str() << endl;
+			int number = 0;
+			int decpos = 1;
+			bool found = false;
+			while (*it >= '0' && *it <='9')
+			{
+				// we found a paragraph notation
+				number += (*it-'0')*decpos;
+				decpos *= 10;
+				++it;
+			}
+			while (*it == ',' || *it == ' ')
+			{
+				// skip in between
+				++it;
+			}
+			if (!number) break;
+			markers.push_back (number);
+		}
+		current.erase(it.base(), current.end());
+		cout << "current now is: " << current << endl;;
+		cout << "markers found: " << endl;
+		for (int i=0; i<markers.size(); ++i) cout << markers[i] << " ";
+
+		if (!markers.empty())
+		{
 			if (!quotes.back().empty()) quotes.back() += "\n";
 			quotes.back() += current;
+			markers.pop_back();
+
+			std::string copy = quotes.back();
+			while (!markers.empty())
+			{
+				markers.pop_back();
+				quotes.push_back(copy);
+			}
 			quotes.push_back(""); // start a new quote
 		}
 		else if (current == "")
